50aeceda51552e077351be5655240da52698bf30
Rene Ventura <venturar729@gmail.com>
2018-12-19 18:47:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,9 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.map do |call|
-    call.upcase + "!"
+    call.capitalize + "!"
   end
 end
-
-puts summon_captain_planet(["heart"])
 
 def long_planeteer_calls# code an argument here
   # Your code here
