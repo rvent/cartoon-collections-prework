03900723464743f0d3606260cd22942c23dcc829
Rene Ventura <venturar729@gmail.com>
2018-12-19 18:30:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts index, dwarf
+  end
 end
 
 def summon_captain_planet# code an argument here
