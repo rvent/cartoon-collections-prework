c08e389745b5b172dc85063192cb6c41d7f9b7f5
Rene Ventura <venturar729@gmail.com>
2018-12-19 18:43:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |call|
+    call.upcase + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
