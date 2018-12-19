9dc0bff3ca29b664918c78563d6c78f807224f12
Rene Ventura <venturar729@gmail.com>
2018-12-19 18:46:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map do |call|
     call.upcase + "!"
   end
-  planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
