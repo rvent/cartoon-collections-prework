6a31a1153df519dbbff6875f599881e90269bd1a
Rene Ventura <venturar729@gmail.com>
2018-12-19 19:06:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,12 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if food.include?(cheese)
+      return food.find(cheese)
+    end
+  end
 end
